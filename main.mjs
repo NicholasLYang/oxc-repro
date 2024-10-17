ccd785073a32cd1f7e18edf677504ec167e4d891
Nicholas Yang <nicholas.yang@vercel.com>
2024-10-17 14:28:30 -0400

    First pass at repro

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import resolve from "oxc-resolver"
+
+console.log("resolving \"./apps/web/nm/@repo/typescript-config/index.js\"")
+console.log(resolve.sync(".", "./apps/web/nm/@repo/typescript-config/index.js"));
+console.log("resolving \"./apps/web/nm/@repo/index.js\"")
+console.log(resolve.sync(".", "./apps/web/nm/@repo/index.js"));
+
